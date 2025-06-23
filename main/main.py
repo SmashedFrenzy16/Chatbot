72f1c9884fed45dd039b7aa41c2d01a04d15aa43
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2025-06-23 21:03:14 +0100

    Update main.py adding basic training to the bot

--- main/main.py
+++ main/main.py
@@ -2,3 +2,16 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 initial_bot = ChatBot("CoffeeBot")
+
+exit_words = ("quit", "exit", "see you later", "talk to you later")
+
+bot_trainer = ListTrainer(initial_bot)
+
+bot_trainer.train([
+    "Hello",
+    "Hello there, what's up?",
+])
+bot_trainer.train([
+    "How do you make coffee?",
+    "You add coffee powder to the hot water, and let it brew!",
+])
